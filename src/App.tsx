1a91a4a50e3a43b170876751b34cd3a0ca06a201
Younes Cherifi <A0483@mydfsp.local>
2025-04-17 10:41:13 +0100

    Restore previous landing page as default route

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 import Editor from "@/pages/Editor";
 import Landing from "@/pages/Landing";
-import Index from "@/pages/Index";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="cv-creator-theme">
       <Router>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/landing" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/editor" element={<Editor />} />
         </Routes>
         <Toaster />
